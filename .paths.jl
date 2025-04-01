ffb8ca7b399b6a7aab0bb60186cc06c8906bc3ae
MatteoPegorin <160166424+MatteoPegorin@users.noreply.github.com>
2025-04-01 20:28:06 +0200

    Update .paths.jl

--- .paths.jl
+++ .paths.jl
@@ -13,12 +13,12 @@ function whoIsThere(PhD)
         println("Matteo is here")
         # my paths are
         path_catalog = "catalogs/"
-        path_output = "output"
+        path_output = ""
     elseif PhD == "Joachim"
         println("Joachim is here")
         # my paths are
         path_catalog = "catalogs/"
-        path_output = "output"
+        path_output = ""
     else
         println("I don't know who you are")
 
