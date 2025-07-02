984c3cf6632b7f4522bbf80f72b06f6053bd8554
Andrea Begnoni <andrea.begnoni42@gmail.com>
2025-07-02 16:37:49 +0200

    fixed bug introduced with previous commit

--- B_grid_plot.jl
+++ B_grid_plot.jl
@@ -5,7 +5,7 @@ using KernelDensity, Statistics
 using Serialization
 using Turing
 using Base.Threads
-using CairoMakie
+using Plots
 
 # include required scripts 
 include("_hierarchical_dist.jl")
@@ -36,7 +36,6 @@ PN_name = pn_order_dic[pno][2]
 network = configs["network_list"][1]
 n_events = configs["n_events"]
 n_median = configs["n_median"]
-needToRun = configs["needToRunAveraging"]
 gridSize = length(mu_vec) * length(sigma_vec) 
 
 # set up folder names
@@ -59,64 +58,67 @@ global_index_total = Dict()
 
 # process all the plots
 
-if needToRun == true
-    idx=0
-    global res = zeros((length(mu_vec), length(sigma_vec)))
-    for i in 1:length(mu_vec)
-        for j in 1:length(sigma_vec)
-            global idx += 1
-            println("\n Analyzing data for index $(idx) out of $(gridSize)")
-            # skip the (0.,0.) case
-            if mu_vec[i] == 0. && sigma_vec[j] == 0.
-                continue
-            end
 
-            mu = mu_vec[i]
-            sigma = sigma_vec[j]
-            header = "grid_PN_$(PN_name)_n_$(idx)_mu_" * string(mu) * "_sigma_" * string(sigma)
-            data_grid_folder_name = data_folder_name * header * "/data/" * network# * "/pn_" * PN_name 
-
-            println(" Mu: ", mu, " Sigma: ", sigma)
-            file_name = data_grid_folder_name * "/global_indices.h5"
-            h5open(file_name, "r") do gi_file
-                global_index_fisher[network] = read(gi_file, "fisher")  
-                global_index_snr[network] = read(gi_file, "snr")
-                global_index_total[network] = read(gi_file, "total")
-            end
-
-            data_grid_folder_name_pno = data_folder_name * header * "/data/" * network * "/pn_" * PN_name
-            filename = data_grid_folder_name_pno * "/single_event_measurements.h5"
-            data = Dict()
-            h5open(filename, "r") do file
-                for key in keys(file)
-                    data[key] = read(file, key)
-                end
-            end
-
-            # extract only valid data-points via global index
-            dphi0_k = data["dphi0_k"][global_index_total[network]]
-            delta_k = data["delta_k"][global_index_total[network]]
-
-            global res[i,j] = reshuffling_bisection(n_median, wrapper_3sigma, dphi0_k, delta_k, sigma)[1]
+idx=0
+global res = zeros((length(mu_vec), length(sigma_vec)))
+for i in 1:length(mu_vec)
+    for j in 1:length(sigma_vec)
+        global idx += 1
+        println("\n Analyzing data for index $(idx) out of $(gridSize)")
+        # skip the (0.,0.) case
+        if mu_vec[i] == 0. && sigma_vec[j] == 0.
+            continue
         end
-    end
 
-    # save data
+        mu = mu_vec[i]
+        sigma = sigma_vec[j]
+        header = "grid_PN_$(PN_name)_n_$(idx)_mu_" * string(mu) * "_sigma_" * string(sigma)
+        data_grid_folder_name = data_folder_name * header * "/data/" * network# * "/pn_" * PN_name 
 
-    h5open(output_folder_name*"results.h5", "w") do file
-        write(file, "results", res)
-    end
-else
-    println("The simulation will not be run!")
-    # load the data
-    file_name = output_folder_name * "results.h5"
-    h5open(file_name, "r") do file
-        global res = read(file, "results")
+        println(" Mu: ", mu, " Sigma: ", sigma)
+        file_name = data_grid_folder_name * "/global_indices.h5"
+        h5open(file_name, "r") do gi_file
+            global_index_fisher[network] = read(gi_file, "fisher")  
+            global_index_snr[network] = read(gi_file, "snr")
+            global_index_total[network] = read(gi_file, "total")
+        end
+
+        data_grid_folder_name_pno = data_folder_name * header * "/data/" * network * "/pn_" * PN_name
+        filename = data_grid_folder_name_pno * "/single_event_measurements.h5"
+        data = Dict()
+        h5open(filename, "r") do file
+            for key in keys(file)
+                data[key] = read(file, key)
+            end
+        end
+
+        # extract only valid data-points via global index
+        dphi0_k = data["dphi0_k"][global_index_total[network]]
+        delta_k = data["delta_k"][global_index_total[network]]
+
+        global res[i,j] = reshuffling_bisection(n_median, wrapper_3sigma, dphi0_k, delta_k, mu, sigma)[1]
     end
 end
 
+# save data
+
+h5open(output_folder_name*"results.h5", "w") do file
+    write(file, "results", res)
+end
+# else
+#     println("The simulation will not be run!")
+#     # load the data
+#     file_name = output_folder_name * "results.h5"
+#     h5open(file_name, "r") do file
+#         global res = read(file, "results")
+#     end
+# end
+
 fig_file_name = plot_folder_name * "grid_plot.pdf"
 println("\nSaving grid plot in: $(fig_file_name)")
+
+
+using CairoMakie
 
 ### leave unchanged
 fontsize_theme = Theme(fontsize = 24)
@@ -151,10 +153,10 @@ ax = Axis(fig[1, 1], xlabel = L"\mu", ylabel = L"\sigma", title = "PN: "* string
 
 # Create a heatmap with the results
 hm = Makie.heatmap!(ax, mu_vec, sigma_vec , log10.(res), colormap = :viridis)
-Colorbar(fig[1, 2], hm,  size = 20,
+Makie.Colorbar(fig[1, 2], hm,  size = 20,
                      ticklabelsize = 20, ticks = (lim, lim_label))
 
-Label(fig[1, 2, Top()], L"n_{\text{events}}", fontsize = labelfontsize)
+Makie.Label(fig[1, 2, Top()], L"n_{\text{events}}", fontsize = labelfontsize)
 
 ax.titlesize = titlefontsize
 ax.xlabelsize = labelfontsize
