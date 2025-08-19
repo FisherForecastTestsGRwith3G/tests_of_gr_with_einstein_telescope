2ce4b5031d4430ddf74aac72610175cb6f142a60
Matteo Pegorin <160166424+MatteoPegorin@users.noreply.github.com>
2025-08-19 12:02:16 +0200

    Bugfix
    Julia does not choose the correct function overload based on the type of keyword arguments.

--- _setup_gr_deviations.jl
+++ _setup_gr_deviations.jl
@@ -36,31 +36,8 @@ function deltaPnNormal(
     Φ_coal::Vector{Float64},
     lambda1::Union{Vector{Float64},Nothing}=nothing,
     lambda2::Union{Vector{Float64},Nothing}=nothing;
-    mu::Float64,
-    sigma::Float64
-    )
-
-    mu = mu*ones(Float64, 10)
-    sigma = sigma*ones(Float64, 10)
-    return deltaPnNormal(mc, η, χ_1, χ_2, dL, θ, ϕ, iota, ψ, tcoal, Φ_coal, lambda1, lambda2, mu=mu, sigma=sigma)
-end
-
-function deltaPnNormal(
-    mc::Vector{Float64}, 
-    η::Vector{Float64},
-    χ_1::Vector{Float64},
-    χ_2::Vector{Float64},
-    dL::Vector{Float64},
-    θ::Vector{Float64},
-    ϕ::Vector{Float64},
-    iota::Vector{Float64},
-    ψ::Vector{Float64},
-    tcoal::Vector{Float64},
-    Φ_coal::Vector{Float64},
-    lambda1::Union{Vector{Float64},Nothing}=nothing,
-    lambda2::Union{Vector{Float64},Nothing}=nothing;
-    mu::Union{Vector{Float64},Nothing}=nothing,
-    sigma::Union{Vector{Float64},Nothing}=nothing
+    mu::Union{Vector{Float64},Float64,Nothing}=nothing,
+    sigma::Union{Vector{Float64},Float64,Nothing}=nothing
     )
 
     n_events = length(mc)
@@ -76,9 +53,17 @@ function deltaPnNormal(
 
     if mu === nothing
         mu = zeros(10)
+    else
+        if isa(mu, Number)
+            mu = mu*ones(Float64, 10)
+        end
     end
     if sigma === nothing
         sigma = zeros(10)
+    else
+        if isa(sigma, Number)
+            sigma = sigma*ones(Float64, 10)
+        end
     end
     
     delta_pn = [
