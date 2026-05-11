021365c5d36c604b468d0cf76e1b80b8a63ed045
Joachim Pomper <joachim.pomper.phy@gmail.com>
2026-05-11 10:45:37 +0200

    fix HLV detector name in _setup_network.jl
    	switch from wrong LHV to HLV

--- create_single_event_datasets/_setup_network.jl
+++ create_single_event_datasets/_setup_network.jl
@@ -66,7 +66,7 @@ end
 # LIGO-Virgo detector networks                                                #
 #-----------------------------------------------------------------------------#
 
-function _setupLHVNetwork(l_asd_path, h_asd_path, v_asd_path, label_suffix)
+function _setupHLVNetwork(l_asd_path, h_asd_path, v_asd_path, label_suffix)
 
     ligo_l = Detector(getCoords(LIGO_L)..., 'L', _readASD(l_asd_path)..., "LIGO_L_$(label_suffix)")
     ligo_h = Detector(getCoords(LIGO_H)..., 'L', _readASD(h_asd_path)..., "LIGO_H_$(label_suffix)")
@@ -78,40 +78,40 @@ end
 """
 Set up the O3a HLV network.
 """
-function setupLHVO3a(asd_dir)
+function setupHLVO3a(asd_dir)
 
     l_asd_path = joinpath(asd_dir, "O3a", "O3-L1-C01_CLEAN_SUB60HZ-1240573680.0_sensitivity_strain_asd.txt")
     h_asd_path = joinpath(asd_dir, "O3a", "O3-H1-C01_CLEAN_SUB60HZ-1251752040.0_sensitivity_strain_asd.txt")
     v_asd_path = joinpath(asd_dir, "O3a", "O3-V1_sensitivity_strain_asd.txt")
 
-    return _setupLHVNetwork(l_asd_path, h_asd_path, v_asd_path, "O3a")
+    return _setupHLVNetwork(l_asd_path, h_asd_path, v_asd_path, "O3a")
 end
 
 """
 Set up the O3b HLV network.
 """
-function setupLHVO3b(asd_dir)
+function setupHLVO3b(asd_dir)
 
     l_asd_path = joinpath(asd_dir, "O3b", "O3-L1-C01_CLEAN_SUB60HZ-1262141640.0_sensitivity_strain_asd.txt")
     h_asd_path = joinpath(asd_dir, "O3b", "O3-H1-C01_CLEAN_SUB60HZ-1262197260.0_sensitivity_strain_asd.txt")
     v_asd_path = joinpath(asd_dir, "O3b", "O3-V1-1265246178_sensitivity_strain_asd.txt")
 
-    return _setupLHVNetwork(l_asd_path, h_asd_path, v_asd_path, "O3b")
+    return _setupHLVNetwork(l_asd_path, h_asd_path, v_asd_path, "O3b")
 end
 
 """
 Set up the pre-O4 estimate HLV network used as pO4/O4a sensitivity estimate.
 """
-function setupLHVpO4(asd_dir)
+function setupHLVpO4(asd_dir)
 
     l_asd_path = joinpath(asd_dir, "O4_preO4estimates", "aligo_O4high.txt")
     h_asd_path = joinpath(asd_dir, "O4_preO4estimates", "aligo_O4high.txt")
     v_asd_path = joinpath(asd_dir, "O4_preO4estimates", "avirgo_O4high_NEW.txt")
 
-    return _setupLHVNetwork(l_asd_path, h_asd_path, v_asd_path, "pO4")
+    return _setupHLVNetwork(l_asd_path, h_asd_path, v_asd_path, "pO4")
 end
 
-setupLHVO3(asd_dir) = setupLHVO3a(asd_dir)
+setupHLVO3(asd_dir) = setupHLVO3a(asd_dir)
 
 #-----------------------------------------------------------------------------# 
 # Select detector network                                                     #
@@ -125,7 +125,7 @@ function getNetwork(network_name)
     psd_data_dir_et  = joinpath(@__DIR__, "psd_data", "et_curves")
     asd_data_dir_lvk = joinpath(@__DIR__, "psd_data", "hlv_curves")
 
-    available_networks = ["ETS", "network_0_15km", "network_45_15km", "LHV", "LHV_O3", "LHV_O3a", "LHV_O3b", "LHV_pO4"]
+    available_networks = ["ETS", "network_0_15km", "network_45_15km", "HLV", "HLV_O3", "HLV_O3a", "HLV_O3b", "HLV_pO4"]
 
     if network_name == "ETS"
         return setupET10kmT(psd_data_dir_et)
@@ -133,12 +133,12 @@ function getNetwork(network_name)
         return setupET15km0(psd_data_dir_et)
     elseif network_name == "network_45_15km"
         return setupET15km45(psd_data_dir_et)
-    elseif network_name == "LHV_O3a"
-        return setupLHVO3a(asd_data_dir_lvk)
-    elseif network_name == "LHV_O3b"
-        return setupLHVO3b(asd_data_dir_lvk)
-    elseif network_name == "LHV_pO4"
-        return setupLHVpO4(asd_data_dir_lvk)
+    elseif network_name == "HLV_O3a"
+        return setupHLVO3a(asd_data_dir_lvk)
+    elseif network_name == "HLV_O3b"
+        return setupHLVO3b(asd_data_dir_lvk)
+    elseif network_name == "HLV_pO4"
+        return setupHLVpO4(asd_data_dir_lvk)
     else 
         throw(ArgumentError("$(network_name) network not available. Use one of these: $(available_networks)"))
     end 
